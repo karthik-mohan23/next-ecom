56ef52a9b8f3882c1edb34a5055d009f37190408
karthik-mohan23 <karthikmohan2310@gmail.com>
2024-08-17 12:23:28 +0530

    fix error caused by `'`

--- src/components/promotionsHeader.tsx
+++ src/components/promotionsHeader.tsx
@@ -2,7 +2,7 @@ function PromotionsHeader() {
   return (
     <div className="bg-blue-700 py-2">
       <p className="text-white text-center w-[90%] mx-auto">
-        🎉 Celebrate India's Independence Day with Us! Use Code{" "}
+        🎉 Celebrate India&apos;s Independence Day with Us! Use Code{" "}
         <strong>FREEDOM78</strong> for 15% Off on checkout! 🇮🇳
       </p>
     </div>
